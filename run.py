7dac167f36660b921140d70e3bb7747f4dbdf7c5
Silvia Saverino <silviasave89@gmail.com>
2023-01-03 11:18:01 +0000

    create models.py file

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from taskmanager import app
 
 if __name__ == "__main__":
     app.run(
-        hotst=os.environ.get("IP"),
+        host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG")
     )
